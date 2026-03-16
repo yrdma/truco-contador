7572c7aad6a7848c2dfbe9b1490db20efc8e68df
yrdma <artuzoygor@gmail.com>
2026-03-16 19:53:05 -0300

    adiciona contador simples com incremento e decremento

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import { Image } from 'react-native'
 
 export default function App() {
+
+const[contador,setContador] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image/>
+      <Text style={{fontSize: 40}}>Marcador</Text>
+      
+
+      <Text>{contador}</Text>
+
+   <View style={styles.buttons}>
+    <View style ={styles.aumento}>
+      <Button  
+      color= "#07a32e"
+      title='+' 
+      onPress={() => setContador (contador + 1)}
+      />
+      </View>
+      <View style ={styles.diminuir}>
+      <Button
+        
+        color= "#ff0000"
+        title='-'
+         onPress={() => contador > 0 && setContador(contador - 1)}
+         />
+  </View>
+   </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +43,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  },buttons:{
+    flexDirection:'row' ,  
+    margin: 10,
+    gap: 10
+  },aumento:{
+    width: 75
+  },diminuir:{
+    width: 75
+  }
 });
+
